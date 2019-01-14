f3141557bcdc74aa963225f174e2406b332afb62
Olajide Fagbuji <ofagbuji@appzonegroup.com>
2019-01-14 08:29:47 +0100

    Updates  to Readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# formy" 
+<div class="jumbotron">
+    <h1>Formy<h1>
+</div>
+<p>A simple approach to creating HTML forms with BOOTSTRAP</p>
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ gulp.task("build", function(done){
     }; 
     buildCfg.Css.map(function(itm){
         var k = Object.keys(itm)[0]
-        //console.log(cssMatcher[k])
         var newLink = `<link rel="stylesheet" type="text/css" href="${itm[k]}" />`;
         gulp.src(["src/index.html"])
         .pipe(replace(cssMatcher[k], newLink))
@@ -19,19 +18,12 @@ gulp.task("build", function(done){
     });
     buildCfg.Scripts.map(function(itm){
         var k = Object.keys(itm)[0]
-        //console.log(scriptMatcher[k])
         var newLink = `<script type="text/javascript" src="${itm[k]}"></script>`;
         gulp.src(["src/index.html"])
         .pipe(replace(scriptMatcher[k], newLink))
         .pipe(gulp.dest('dist/'));        
     })
-    /*
-    for(let i=0; i<3; i++){
-        gulp.src("src/index.html")
-        .pipe(replace('bar', 'foo'))
-        .pipe(gulp.dest('dist/'));
-    }
-    */
+
 
     done(); 
 })
